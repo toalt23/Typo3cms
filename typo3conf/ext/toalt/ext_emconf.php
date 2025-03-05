e79e939670337fc5f40514b98a2b41f45e9de400
Tobias <to.alt@web.de>
2025-03-05 20:34:52 +0100

    ext_emconf.php

--- typo3conf/ext/toalt/ext_emconf.php
+++ typo3conf/ext/toalt/ext_emconf.php
@@ -7,11 +7,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Tobias Alt',
     'author_email' => 'to.alt@web.de',
     'author_company' => 'some company',
-    'state' => 'alpha',
+    'state' => 'stable',
     'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0',
+            'typo3' => '12.4.27',
         ],
         'conflicts' => [
         ],
