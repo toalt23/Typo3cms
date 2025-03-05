9d43883c8abc36de824cf3320c71d43a9adbfa17
Tobias <to.alt@web.de>
2025-03-05 20:11:27 +0100

    ext_emconf.php

---
+++ typo3conf/ext/toaltext/ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'toaltext',
+    'description' => 'Tobias Alt Playground',
+    'category' => 'Template',
+    'author' => 'Tobias Alt',
+    'author_email' => 'to.alt@web.de',
+    'author_company' => 'toalt',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
