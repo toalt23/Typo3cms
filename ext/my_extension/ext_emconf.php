3050bae7768d5b0831f9aef981d3a40df980b2e0
Tobias <to.alt@web.de>
2025-03-05 21:57:43 +0100

    ext_emconf.php

---
+++ ext/my_extension/ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'My own Extension',
+    'description' => 'Typo3 Playground',
+    'category' => 'template',
+    'author' => 'Tobias Alt',
+    'author_email' => 'to.alt@web.de',
+    'author_company' => 'some company',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
