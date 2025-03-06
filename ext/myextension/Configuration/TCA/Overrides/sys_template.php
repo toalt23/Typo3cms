d3e6e326d5d549bd4c531556fc524d18acd6c1ab
Tobias <to.alt@web.de>
2025-03-06 12:47:26 +0100

    sys_template.php

--- ext/myextension/Configuration/TCA/Overrides/sys_template.php
+++ ext/myextension/Configuration/TCA/Overrides/sys_template.php
@@ -4,9 +4,6 @@ call_user_func(function()
 {
     $extensionKey = 'myextension';
 
-    /**
-     * Default TypoScript
-     */
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         $extensionKey,
         'Configuration/TypoScript',
