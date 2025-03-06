211af0947b050986442db35c034c0ead6cfd83d4
Tobias <to.alt@web.de>
2025-03-06 12:13:18 +0100

    ext_emconf.php

---
+++ ext/my_extension/Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,18 @@
+<?php
+
+defined("TYPO3") or die();
+
+call_user_func(function () {
+
+    $extensionKey = "my_extension";
+
+    /**
+     * Default TypoScript
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        $extensionKey,
+        'Configuration/TypoScript',
+        'Some template'
+    );
+
+});
