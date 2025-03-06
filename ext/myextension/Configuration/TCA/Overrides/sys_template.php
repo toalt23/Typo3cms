8a38dfa1ecf8140bce09f2d4e714951f340216f7
Tobias <to.alt@web.de>
2025-03-06 12:45:54 +0100

    sys_template.php

--- ext/myextension/Configuration/TCA/Overrides/sys_template.php
+++ ext/myextension/Configuration/TCA/Overrides/sys_template.php
@@ -10,6 +10,6 @@ call_user_func(function()
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         $extensionKey,
         'Configuration/TypoScript',
-        'Some descriptive title'
+        'Tobias Alt Extension'
     );
 });
