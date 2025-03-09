6e70fdcb1321a418fbfd5fa2ceadc5b8ff9c09b1
Tobias <to.alt@web.de>
2025-03-09 19:10:09 +0100

    configure plugin

--- ext/myextension/Configuration/TCA/Overrides/tt_content.php
+++ ext/myextension/Configuration/TCA/Overrides/tt_content.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 defined('TYPO3') or die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'myextension'
+    'myextension',
     'Demo Plugin',
     'demo plugin showcase'
 );
