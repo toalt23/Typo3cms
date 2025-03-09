9eb1e8304e1e6e5324ff855179f5884f3f70065a
Tobias <to.alt@web.de>
2025-03-09 20:45:26 +0100

    configure plugin

--- ext/myextension/ext_localconf.php
+++ ext/myextension/ext_localconf.php
@@ -1,2 +1,13 @@
 <?php
 
+declare (strict_types=1);
+
+defined('TYPO3') or die();
+
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
+ExtensionUtility::configurePlugin(
+    'myextension',
+    'DemoPlugin',
+    [\Tobias\MyExtension\Classes\Controller\DemoController::class => 'show']
+);
