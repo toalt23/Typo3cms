66611349000f7b56c26ca842c111ae33720c77c6
Tobias <to.alt@web.de>
2025-03-09 20:41:25 +0100

    configure plugin

--- ext/myextension/Classes/Controller/DemoController.php
+++ ext/myextension/Classes/Controller/DemoController.php
@@ -1,2 +1,19 @@
 <?php
 
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use Psr\Http\Message\ResponseInterface;
+
+class DemoController extends ActionController {
+
+    public function showAction(): ResponseInterface
+    {
+
+        $testContent = 'Hello World from an extabse plugin';
+
+        $this->view->assign('test',$testContent);
+
+        return $this->htmlResponse();
+
+    }
+
+}
